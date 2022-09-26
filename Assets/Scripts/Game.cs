85d8bb0b2e9f0cfee22de344c1721069d0d241cb
web3ten0 <web3ten0@gmail.com>
2022-09-26 20:48:14 +0900

    Cubeでピラミッド作成

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private GameObject _cube_obj;
+    private GameObject _bomb_obj;
+
+    private float _cube_scale = 0.3f;
+    private int _cube_steps_num = 10;
+    private Vector3 _tower_center;
+
+    void Awake()
     {
-        
+      _cube_obj = (GameObject)Resources.Load("Prefabs/Cube");
+      _bomb_obj = (GameObject)Resources.Load("Prefabs/Bomb");
+      _tower_center =  new Vector3(0, _cube_scale/2f, 0);
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+      for (int i = _cube_steps_num; i > 0; i--)
+      {
+        Vector3 left_top = new Vector3(
+          _tower_center.x + _cube_scale * (i-1),
+          _tower_center.y + _cube_scale * (_cube_steps_num - i),
+          _tower_center.z + _cube_scale * (i-1)
+        );
+        Debug.Log("left_top: " + left_top);
+        int cube_num = i * 2 - 1; 
+        Debug.Log("cube num: " + cube_num);
+        for (int row = 0; row < cube_num; row++)
+        {
+          for (int col = 0; col < cube_num; col++)
+          {
+            Vector3 center = left_top;
+            center.z -= _cube_scale * row;
+            center.x -= _cube_scale * col;
+            Instantiate(_cube_obj, center, Quaternion.identity);
+          }
+        }
+      }
+    }
+
     void Update()
     {
         
